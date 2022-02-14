2065d28e97ff3ab8884d3619e98f0169806788ae
jkimbxv <68555291+jkimbxv@users.noreply.github.com>
2022-02-14 23:16:56 +0000

    keeping up with class

--- flask/feb14.py
+++ flask/feb14.py
@@ -1,23 +1,49 @@
-from flask import Flask
+from flask import Flask, request 
 from flask import render_template
 
 import random
 
 app = Flask(__name__)
 
-#example of route that passes data to a render_template
+
+# Example of a route that passes
+# data to a template
 @app.route("/rand")
 def randomnumber():
-    i = random.randrange(100)
-    return render_template("lucky.html",number=i)
+  i = random.randrange(100)
+  return render_template("lucky.html",number = i )
 
-#the root route
+# the "root" route
 @app.route("/")
 def index():
-    return "<h1>hello world from jiyoon</h1>"
+  return "<h1>Hello World from Repl.it!</h1>"
 
+# return a template 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+  return render_template("about.html")
 
-app.run(host="0.0.0.0",port = 5000, debug=True)
+# example of static content
+# like an image or including css
+@app.route("/image_css")
+def image_css():
+  return render_template("image_css.html")
+  
+@app.route("/form_demo",methods=['GET','POST'])
+def form_demo():
+  # GET is when we just load the page in our browser
+  # POST is when we click the button 
+  if request.method=="GET":
+    return render_template("form_demo.html")
+  else:
+    # here we clicked the button
+    # so we can check the form data
+    name = request.form['username']
+    pw = request.form['password']
+    print(name,pw)
+    
+    return "<h1>YOU CLICKED THE BUTTON</h1>"
+
+
+  
+app.run(host="0.0.0.0",port=5000,debug=True)
