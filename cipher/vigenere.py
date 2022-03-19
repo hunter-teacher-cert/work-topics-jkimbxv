7969de767907657dd7e5cb6685aa48d7f643174f
Jiyoon Kim <jkim@bronxvilleschool.org>
2022-03-19 12:04:06 -0400

    got encryption working

--- cipher/vigenere.py
+++ cipher/vigenere.py
@@ -1,19 +1,19 @@
-message = "computerscience"
+secret = "computerscience"
 key = "bcd"
-encrypted=""
 #chr(ASCIICODE) --> character
 #ord(CHARACTER) --> ASCII value
 
-message.upper()
-key.upper()
-
-i = 0
-while (i<len(message)):
-    iKey = i%len(key) #index of key string
-    p = ord(message[i])-65 #ord to convert chr to ascii, 65 due to A's ascii code
-    d = ord(key[iKey])-65
-    c = chr(((p+d)%26)+65) #add 65 again to get to letters in ascii code
-    encrypted+=c
-    i+=1
-
-print(encrypted)
+def encrypt(message,key):
+    encrypted=""
+    message = message.upper()
+    key = key.upper()
+    i = 0
+    while (i<len(message)):
+        iKey = i%len(key) #index of key string
+        p = ord(message[i])-65 #ord to convert chr to ascii, 65 due to A's ascii code
+        d = ord(key[iKey])-65
+        c = chr(((p+d)%26)+65) #add 65 again to get to letters in ascii code
+        encrypted+=c
+        i+=1
+    return encrypted
+print(encrypt(secret,key))
