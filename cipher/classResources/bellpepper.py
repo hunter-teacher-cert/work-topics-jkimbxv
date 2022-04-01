8f8f2204765171c03424d025b05314d8288c1816
Jiyoon Kim <jkim@bronxvilleschool.org>
2022-04-01 17:53:38 -0400

    updated hw templates

--- cipher/classResources/bellpepper.py
+++ cipher/classResources/bellpepper.py
@@ -1,1 +1,36 @@
-#tiffany's code goes here:
+#In Class Demo
+#encrypt letters such that capital Z shifted 1 is capital A and lowercase z shifted 1 is lowercase a
+def encrypt(text,s):
+  #traverse the plain text
+  for i in range(len(text)):
+    char = text[i]
+    # Encrypt uppercase characters in plain text
+    if (char.isupper()):
+      #convert the letter into unicode
+      #make A unicode 0 by subtracting 65
+      #add shift
+      #allow wraparound by mod 26
+      #make A's unicode 65 again by adding 65
+      #convert back from unicode to letter
+      result += chr((ord(char) - 65 + s) % 26 + 65)
+    else:
+    # Async Work: Encrypt lowercase characters in plain text
+
+#enter alphabetic text without spaces and a desired shift
+text = "AZaz"
+s = 27
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(s))
+print("Encrypted Text: " + encrypt(text,s))
+
+#Homework Bell Pepper Version: Write a function that decrypts a symmetric cipher with a given shift
+text = " "
+s = 27
+
+def decrypt(text,s):
+  return result
+
+print("Encrypted Text : " + text)
+print("Shift pattern : " + str(s))
+print("Decrypted Text: " + decrypt(text,s))
